52cad39168048e9268157a6bededb5117682e9bd
peppizza <spencervess@gmail.com>
2024-05-18 14:26:02 -0400

    Add format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,38 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.cmake_format,
-				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.diagnostics.cmake_lint,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  lazy = false,
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
-	keys = {
-		{ "<leader>gf", vim.lsp.buf.format, desc = "Format current buffer" },
-	},
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.cmake_format,
+        require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.diagnostics.cmake_lint,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+  keys = {
+    { "<leader>gf", vim.lsp.buf.format, desc = "Format current buffer" },
+  },
 }
