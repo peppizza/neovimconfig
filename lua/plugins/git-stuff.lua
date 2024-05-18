7659b7f335f9d6ad8b4bed836cc5a67f219d31ac
peppizza <spencervess@gmail.com>
2024-05-18 12:08:31 -0400

    Add in easy split navigation as well as make most keymaps silent

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -7,8 +7,8 @@ return {
 		config = function()
 			require("gitsigns").setup()
 
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {silent = true})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {silent = true})
 		end,
 	},
 }
