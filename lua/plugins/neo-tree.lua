7495a5c822d900013ca8238f636f95cc0f899ec7
peppizza <spencervess@gmail.com>
2024-05-18 14:20:08 -0400

    Formatting

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,21 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  keys = {
-    { "<C-n>", "<cmd>Neotree filesystem reveal left<CR>", silent = true, desc = "Reveal and jump to Neotree" },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        never_show = { ".git" }
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{ "<C-n>", "<cmd>Neotree filesystem reveal left<CR>", silent = true, desc = "Reveal and jump to Neotree" },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = true,
+				never_show = { ".git" },
+			},
+		},
+	},
 }
