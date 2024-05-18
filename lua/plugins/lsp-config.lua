b3ed76bc8c5b12c6fb3e73080a68f8f51492bc80
peppizza <spencervess@gmail.com>
2024-05-18 11:02:38 -0400

    Add completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,10 +16,18 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
-      lspconfig.rust_analyzer.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
