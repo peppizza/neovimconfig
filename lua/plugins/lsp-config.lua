bb35f88eb1f018faf884e51b95c8ac54b35b187d
peppizza <spencervess@gmail.com>
2024-05-19 14:22:21 -0400

    Formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,58 +1,58 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = true,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		ensure_installed = {
-			"lua_ls",
-			"tsserver",
-			"rust_analyzer",
-			"stylua",
-			"prettier",
-			"cmakelang",
-			"cmakelint",
-			"eslint_d",
-		},
-		config = true,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    ensure_installed = {
+      "lua_ls",
+      "tsserver",
+      "rust_analyzer",
+      "stylua",
+      "prettier",
+      "cmakelang",
+      "cmakelint",
+      "eslint_d",
+    },
+    config = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ccls.setup({
-				init_options = {
-					compilationDatabaseDirectory = "build",
-					index = {
-						threads = 0,
-					},
-					clang = {
-						excludeArgs = { "-frounding-math" },
-					},
-				},
-				capabilities = capabilities,
-			})
-			lspconfig.cmake.setup({
-				capabilities = capabilities,
-			})
-		end,
-		keys = {
-			{ "K", vim.lsp.buf.hover, desc = "LSP hover" },
-			{ "gd", vim.lsp.buf.definition, desc = "Jump to definition" },
-			{ "gr", vim.lsp.buf.references, desc = "Search for references" },
-			{ "<leader>ca", vim.lsp.buf.code_action, mode = { "n", "v" }, desc = "LSP code actions" },
-		},
-	},
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ccls.setup({
+        init_options = {
+          compilationDatabaseDirectory = "build",
+          index = {
+            threads = 0,
+          },
+          clang = {
+            excludeArgs = { "-frounding-math" },
+          },
+        },
+        capabilities = capabilities,
+      })
+      lspconfig.cmake.setup({
+        capabilities = capabilities,
+      })
+    end,
+    keys = {
+      { "K",          vim.lsp.buf.hover,       desc = "LSP hover" },
+      { "gd",         vim.lsp.buf.definition,  desc = "Jump to definition" },
+      { "gr",         vim.lsp.buf.references,  desc = "Search for references" },
+      { "<leader>ca", vim.lsp.buf.code_action, mode = { "n", "v" },           desc = "LSP code actions" },
+    },
+  },
 }
