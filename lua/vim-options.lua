fa73f4a7eb7a6472a2820e99d4097065ba8dceb8
peppizza <spencervess@gmail.com>
2024-05-19 11:38:33 -0400

    Added relative line numbers and autoscrolling

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set noshowmode")
+vim.cmd("set scrolloff=20")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
